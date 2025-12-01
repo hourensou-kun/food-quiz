639100bd2a70cca03506003cf48a8e92309bded0
spinach <rtsunashima2010@gmail.com>
2025-12-01 23:48:58 +0900

    Update script.js

--- script.js
+++ script.js
@@ -221,7 +221,7 @@ judgeImg.src = isCorrect
   show("judge-screen");
 
   // 1秒後に答えあわせへ
-  setTimeout(() => showAnswer(q), 1000);
+  setTimeout(() => showAnswer(q), 2000);
 }
 
 // ---- 答えあわせ画面（クイズ1）----

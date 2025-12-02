4fcbdc2b7a44da4395f6ab3ca33d026c4e489c69
spinach <rtsunashima2010@gmail.com>
2025-12-02 19:36:20 +0900

    Update script.js

--- script.js
+++ script.js
@@ -535,7 +535,7 @@ function renderResult() {
 
   // 新しいスコアUI書き込み
   document.getElementById("score-total").textContent =
-    `${quizData.length}問中…`;
+    `${quizData.length}<ruby><rb>門</rb><rt>もん</rt></ruby><ruby><rb>中</rb><rt>ちゅう</rt></ruby>…`;
 
   document.getElementById("score-number").textContent = score;
 
